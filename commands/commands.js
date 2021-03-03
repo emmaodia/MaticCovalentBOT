57f0daeb1b4030b37ed1a8bf206c824a07e194b8
emmaodia <odia.emma@gmail.com>
2021-03-03 21:14:25 +0100

    fix: updated some commands that a user can run

--- commands/commands.js
+++ commands/commands.js
@@ -3,7 +3,23 @@ module.exports = {
   description: "Hello Geeks!",
   execute(message, args) {
     message.channel.send(
-      "You can run the following commands to interact with the BOT: \n\nHello\nCommands\nCreator"
+      `To interact with the BOT, kindly run the following commands:
+      
+    To view a Token Balance for an address: 
+          /token {token_name}
+          e.g: /token molochdao
+
+    To view a Token transaction for an address:
+          /tran {token_name or address_string}
+
+    To get the latest Block Height:
+          /block
+    
+    To view a SINGULAR transaction for an address:
+            /tran {address_string}
+
+    To view NFT token IDs for an address:
+            /tran {token_name or address_string}`
     );
   },
 };

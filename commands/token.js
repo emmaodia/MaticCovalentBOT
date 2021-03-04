13919a8bfeb887bc07458ac850020bdc24d1a0c4
emmaodia <odia.emma@gmail.com>
2021-03-04 18:28:55 +0100

    chore: removed comented code

--- commands/token.js
+++ commands/token.js
@@ -8,25 +8,6 @@ module.exports = {
   name: "token",
   description: "Hello Geeks!",
   execute: async (message, args) => {
-    //   message.channel.send(
-    //     `To interact with the BOT, kindly run the following commands:
-
-    //   To view a Token Balance for an address:
-    //         /token {token_name}
-    //         e.g: /token molochdao
-
-    //   To view a Token transaction for an address:
-    //         /tran {token_name or address_string}
-
-    //   To get the latest Block Height:
-    //         /block
-
-    //   To view a SINGULAR transaction for an address:
-    //           /tran {address_string}
-
-    //   To view NFT token IDs for an address:
-    //           /tran {token_name or address_string}`
-    //   );
     if (!args.length) {
       return message.channel.send("You need to supply a search term!");
     }
